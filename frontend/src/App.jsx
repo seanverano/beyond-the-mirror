c1de0b93d1d0503c4d3aed63f0757b174e76fbdb
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-11-12 13:00:30 +0800

    Install tailwind packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import AuthPage from "./pages/AuthPage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <AuthPage />
+      <h1 className="text-[#23232] bg-red-700">test</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,9 @@
+const AuthPage = () => {
+  return (
+    <>
+      <div className="bg-red text-blue-50">Hi test</div>
+    </>
+  );
+};
+
+export default AuthPage;
